37995491e48ea9c2bf0ea0b56bff7e640b96f5a2
rahulsd380 <rahulsd380@gmail.com>
2025-07-24 12:07:31 +0600

    Added navbar

--- src/app/(mainLayout)/layout.tsx
+++ src/app/(mainLayout)/layout.tsx
@@ -1,9 +1,13 @@
+import Footer from '@/components/Shared/Footer/Footer';
+import Navbar from '@/components/Shared/Navbar/Navbar';
 import{ ReactNode } from 'react';
 
 const MainLayout = ({children} : {children: ReactNode}) => {
     return (
         <div>
+            <Navbar/>
             {children}
+            <Footer/>
         </div>
     );
 };
--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,10 +1,62 @@
+"use client";
+import Container from "@/components/Reusable/Container/Container";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  const pathname = usePathname();
+  const navlinks = [
+    {
+      label: "Home",
+      path: "/",
+    },
+    {
+      label: "About Us",
+      path: "/about-us",
+    },
+    {
+      label: "Blogs",
+      path: "/blogs",
+    },
+    {
+      label: "Contact Us",
+      path: "/contact-us",
+    },
+  ];
+  return (
+    <div className="bg-neutral-35 font-Satoshi">
+      <Container>
+        <div className="flex items-center justify-between py-5">
+          <Link
+            href={"/"}
+            className="text-white font-extrabold font-Satoshi text-2xl"
+          >
+            Klick Magnet
+          </Link>
+
+          <div className="flex items-center gap-5">
+            {navlinks?.map((item) => (
+              <Link
+                key={item?.label}
+                href={item?.path}
+                className={`text-base font-medium leading-5 hover:underline ${
+                  pathname === item?.path ? "text-white" : "text-neutral-20"
+                }`}
+              >
+                {item?.label}
+              </Link>
+            ))}
+            <Link
+              href={"/"}
+              className="text-white font-medium p-[10px] rounded-lg border border-white"
+            >
+              Get Started
+            </Link>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Navbar;
