f8ca3ab47ee5455ef7bd5249903ca1834b9edf0f
rahulsd380 <rahulsd380@gmail.com>
2025-07-24 11:19:49 +0600

    Added images and icons

---
+++ public/assets/index.ts
@@ -0,0 +1,15 @@
+import heroImage from "./images/hero-img.svg";
+import dummyBlogImage from "./images/blog-dummy-image.png";
+
+import downArrow from "./icons/down-arrow.svg";
+import rightArrow from "./icons/right-arrow.svg";
+
+export const IMAGES = {
+  heroImage,
+  dummyBlogImage,
+};
+
+export const ICONS = {
+  downArrow,
+  rightArrow,
+};
